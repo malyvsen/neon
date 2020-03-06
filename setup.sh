6be995a45ed5a210fc7d281b194a97590489aea5
malyvsen <5940672+malyvsen@users.noreply.github.com>
2020-03-06 22:26:13 +0100

    add pyenv setup

--- setup.sh
+++ setup.sh
@@ -2,4 +2,5 @@
 source "$(dirname $0)/setup/dirs.sh" &&
 source "$(dirname $0)/setup/files.sh" &&
 source "$(dirname $0)/setup/dotfiles.sh" &&
-source "$(dirname $0)/setup/install.sh"
+source "$(dirname $0)/setup/install.sh" &&
+source "$(dirname $0)/setup/ai.sh"
