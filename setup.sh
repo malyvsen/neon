573aa5852281600fad6240e03ca88b764aee97c1
malyvsen <5940672+malyvsen@users.noreply.github.com>
2020-05-01 19:40:33 +0200

    fix: reorder setup to avoid overwriting dotfiles

--- setup.sh
+++ setup.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 source "$(dirname $0)/setup/dirs.sh" &&
 source "$(dirname $0)/setup/files.sh" &&
-source "$(dirname $0)/setup/dotfiles.sh" &&
 source "$(dirname $0)/setup/install.sh" &&
 source "$(dirname $0)/setup/vscode.sh" &&
 source "$(dirname $0)/setup/ai.sh" && 
-source "$(dirname $0)/setup/grub-theme.sh"
+source "$(dirname $0)/setup/grub-theme.sh" &&
+source "$(dirname $0)/setup/dotfiles.sh"
