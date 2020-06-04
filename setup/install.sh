b2d03892949a8fa764ce914642b01a5f66e89ea6
malyvsen <5940672+malyvsen@users.noreply.github.com>
2020-06-04 19:51:43 +0200

    chore: change zsh prompt to spaceship

--- setup/install.sh
+++ setup/install.sh
@@ -9,4 +9,6 @@ pamac install code &&
 
 pamac install zsh zsh-autosuggestions &&
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" &&
-git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions &&
+git clone https://github.com/denysdovhan/spaceship-prompt.git "$ZSH_CUSTOM/themes/spaceship-prompt" &&
+ln -s "$ZSH_CUSTOM/themes/spaceship-prompt/spaceship.zsh-theme" "$ZSH_CUSTOM/themes/spaceship.zsh-theme"
