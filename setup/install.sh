0b026ed9298ae91a29820d0cebc5cb7997085543
malyvsen <5940672+malyvsen@users.noreply.github.com>
2020-06-04 20:26:34 +0200

    feat: install oh-my-zsh and plugins through relevant packages

--- setup/install.sh
+++ setup/install.sh
@@ -7,8 +7,7 @@ pamac install firefox &&
 pamac install geary telegram-desktop caprine &&
 pamac install code &&
 
-pamac install zsh zsh-autosuggestions &&
-sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" &&
-git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions &&
-git clone https://github.com/denysdovhan/spaceship-prompt.git "$ZSH_CUSTOM/themes/spaceship-prompt" &&
-ln -s "$ZSH_CUSTOM/themes/spaceship-prompt/spaceship.zsh-theme" "$ZSH_CUSTOM/themes/spaceship.zsh-theme"
+pamac install zsh &&
+pamac build oh-my-zsh-git &&
+pamac install zsh-autosuggestions &&
+pamac build spaceship-prompt-git
