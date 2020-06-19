48b7092494dc7315d1d931a685019d28e2f51912
malyvsen <5940672+malyvsen@users.noreply.github.com>
2020-06-19 19:00:21 +0200

    use poetry instead of pyenv

--- setup.sh
+++ setup.sh
@@ -3,6 +3,5 @@ source "$(dirname $0)/setup/dirs.sh" &&
 source "$(dirname $0)/setup/files.sh" &&
 source "$(dirname $0)/setup/install.sh" &&
 source "$(dirname $0)/setup/vscode.sh" &&
-source "$(dirname $0)/setup/pyenv.sh" && 
 source "$(dirname $0)/setup/grub-theme.sh" &&
 source "$(dirname $0)/setup/dotfiles.sh"
