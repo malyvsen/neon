1d1195638c1acd85bc08a01bd34e2df7adec78a0
Mikołaj Bocheński <5940672+malyvsen@users.noreply.github.com>
2023-03-24 21:25:27 +0100

    feature: install python via pyenv and add poetry

--- setup/install.sh
+++ setup/install.sh
@@ -2,10 +2,14 @@
 echo "Installing additional programs..." &&
 
 pamac install exa bat &&
-pamac install python-pip python-poetry &&
 pamac install firefox &&
 pamac install telegram-desktop caprine &&
 pamac install code &&
+
+pamac install pyenv &&
+pyenv install 3 &&
+pyenv shell $(pyenv latest 3) &&
+curl -sSL https://install.python-poetry.org | python3 - &&
 
 pamac install zsh && sudo chsh -s /bin/zsh $USER &&
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" &&
