c8614546cbcb991af076c2f73f7e9b3060ab9293
malyvsen <5940672+malyvsen@users.noreply.github.com>
2024-03-10 18:16:18 +0100

    feat: use starship instead of spaceship

--- setup/install.sh
+++ setup/install.sh
@@ -1,18 +1,14 @@
 #!/bin/bash
 echo "Installing additional programs..." &&
 
-pamac install base-devel &&
-pamac install exa bat &&
-pamac install firefox &&
-pamac install telegram-desktop &&
+pamac install base-devel eza bat firefox telegram-desktop zsh zsh-autosuggestions starship &&
 pamac build visual-studio-code-bin &&
 
 pamac install pyenv &&
+eval "$(pyenv init -)" &&
 pyenv install 3 &&
 pyenv shell $(pyenv latest 3) &&
 curl -sSL https://install.python-poetry.org | python3 - &&
 
-pamac install zsh && sudo chsh -s /bin/zsh $USER &&
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" &&
-pamac install zsh-autosuggestions &&
-pamac build spaceship-prompt
+sudo chsh -s /bin/zsh $USER &&
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
