6500b30ef7dfdeada616f0ff092f6bd7182208df
malyvsen <5940672+malyvsen@users.noreply.github.com>
2024-03-10 20:25:15 +0100

    perf: install pyenv simultaneously with other packages

--- setup/install.sh
+++ setup/install.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 echo "Installing additional programs..." &&
 
-pamac install base-devel eza bat firefox telegram-desktop zsh zsh-autosuggestions starship gnome-shell-extension-gsconnect &&
+pamac install base-devel eza bat pyenv firefox telegram-desktop zsh zsh-autosuggestions starship gnome-shell-extension-gsconnect &&
 echo "Installed GSConnect for clipboard sharing. Enable the GNOME extension and pair manually." &&
 pamac build --no-confirm visual-studio-code-bin &&
 
-pamac install pyenv &&
 eval "$(pyenv init -)" &&
 pyenv install 3 &&
 pyenv shell $(pyenv latest 3) &&
