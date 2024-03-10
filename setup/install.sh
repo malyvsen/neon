10a9e7ff95ab6c9101f9d7a40f3ef8e32c393f15
malyvsen <5940672+malyvsen@users.noreply.github.com>
2024-03-10 20:24:11 +0100

    feat: add info message on GSConnect

--- setup/install.sh
+++ setup/install.sh
@@ -2,6 +2,7 @@
 echo "Installing additional programs..." &&
 
 pamac install base-devel eza bat firefox telegram-desktop zsh zsh-autosuggestions starship gnome-shell-extension-gsconnect &&
+echo "Installed GSConnect for clipboard sharing. Enable the GNOME extension and pair manually." &&
 pamac build --no-confirm visual-studio-code-bin &&
 
 pamac install pyenv &&
