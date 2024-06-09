3a40f9eacd8a8e88aadad13d958e5a45834548bc
malyvsen <5940672+malyvsen@users.noreply.github.com>
2024-06-09 15:42:03 +0200

    feat: install & symlink emoji font

--- setup/install.sh
+++ setup/install.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 echo "Installing additional programs..." &&
 
-pamac install --no-confirm base-devel eza bat pyenv firefox telegram-desktop zsh zsh-autosuggestions starship gnome-shell-extension-gsconnect docker docker-compose &&
+pamac install --no-confirm base-devel eza bat pyenv firefox telegram-desktop zsh zsh-autosuggestions starship gnome-shell-extension-gsconnect docker docker-compose ttf-twemoji &&
+echo "Installed GSConnect for clipboard sharing. Enable the GNOME extension and pair manually." &&
 sudo systemctl enable docker.service &&
 sudo usermod -aG docker $USER &&
-echo "Installed GSConnect for clipboard sharing. Enable the GNOME extension and pair manually." &&
+sudo ln -sf /usr/share/fontconfig/conf.avail/75-twemoji.conf /etc/fonts/conf.d/75-twemoji.conf &&
 
 pamac build --no-confirm visual-studio-code-bin &&
 
