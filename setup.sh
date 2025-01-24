86b655647bb69d102fa51dc3342aa5c3b67cc5a7
Mikołaj Bocheński <5940672+malyvsen@users.noreply.github.com>
2025-01-24 12:52:01 +0100

    fix: create parent directories if needed in setup.sh

--- setup.sh
+++ setup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-mkdir "$HOME/creative/software/systems" &&
+mkdir -p "$HOME/creative/software/systems" &&
 cd "$HOME/creative/software/systems" &&
 git clone https://github.com/malyvsen/neon.git &&
 cd neon &&
