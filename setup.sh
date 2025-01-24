ae5ac136bdd1fe53ca462655c853995ec1198ba3
malyvsen <5940672+malyvsen@users.noreply.github.com>
2025-01-24 21:21:00 +0100

    feat: configure git

--- setup.sh
+++ setup.sh
@@ -4,6 +4,7 @@ cd "$HOME/creative/software/systems" &&
 git clone https://github.com/malyvsen/neon.git &&
 cd neon &&
 source "$(dirname $0)/setup/dirs.sh" &&
+source "$(dirname $0)/setup/git.sh" &&
 source "$(dirname $0)/setup/install.sh" &&
 source "$(dirname $0)/setup/ssh.sh" &&
 source "$(dirname $0)/setup/dotfiles.sh"
---
+++ setup/git.sh
@@ -0,0 +1,5 @@
+git config --global user.name malyvsen &&
+git config --global user.email 5940672+malyvsen@users.noreply.github.com &&
+git config --global init.defaultBranch main &&
+git config --global pull.rebase true &&
+git config --global push.autoSetupRemote true
